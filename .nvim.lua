c313594d41ece878caf493264d45a71b33038adb
Aiden Green <70248258+blaqat@users.noreply.github.com>
2024-08-02 16:24:17 -0400

    Quet Light Changes

---
+++ .nvim.lua
@@ -0,0 +1,17 @@
+local function turn_off_gui_colors()
+	vim.cmd("set notermguicolors")
+end
+
+return {
+	theme = "PaperColor",
+	ui = "light",
+	font = {
+		family = "Nova Nerd Font",
+		size = 15,
+		fallbacks = { "Mononoki Nerd Font" },
+	},
+	line_spacing = 3,
+	lua = {
+		turn_off_gui_colors,
+	},
+}
