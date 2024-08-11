041eaad9864df0105062a2ecd970b7e99ad737a0
Aiden Green <70248258+blaqat@users.noreply.github.com>
2024-08-10 23:37:05 -0400

    Aylin

--- .nvim.lua
+++ .nvim.lua
@@ -3,15 +3,19 @@ local function turn_off_gui_colors()
 end
 
 return {
-	theme = "PaperColor",
-	ui = "light",
+	-- theme = "PaperColor",
+	-- ui = "light",
+	-- font = {
+	-- 	family = "Nova Nerd Font",
+	theme = "aylin",
+	ui = "dark",
 	font = {
-		family = "Nova Nerd Font",
+		family = "Maple Mono",
 		size = 15,
 		fallbacks = { "Mononoki Nerd Font" },
 	},
 	line_spacing = 3,
-	lua = {
-		turn_off_gui_colors,
-	},
+	-- lua = {
+	-- 	turn_off_gui_colors,
+	-- },
 }
