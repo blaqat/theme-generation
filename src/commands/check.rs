6772f886a2e4e4d4290cce2e9b4335ebaf796d45
Aiden Green <70248258+blaqat@users.noreply.github.com>
2024-09-04 12:42:23 -0400

    Check Feature Idiomized

--- src/commands/check.rs
+++ src/commands/check.rs
@@ -6,16 +6,6 @@ use json_patch;
 
 use serde_json::{json, Map, Value};
 
-trait HasDepth {
-    fn has_depth(&self) -> bool;
-}
-
-impl HasDepth for Value {
-    fn has_depth(&self) -> bool {
-        self.is_object() || self.is_array()
-    }
-}
-
 const DNE: &'static str = "DNE";
 
 #[derive(Debug)]
@@ -25,46 +15,42 @@ struct DiffInfo {
 }
 
 impl DiffInfo {
-    fn merge(&mut self, other: &mut DiffInfo) -> Self {
-        let mut new_diffs = self.diffs.clone();
-        new_diffs.append(&mut other.diffs);
-        new_diffs.dedup();
+    fn merge(mut self, other: DiffInfo) -> Self {
+        self.diffs.extend(other.diffs);
+        self.diffs.sort();
+        self.diffs.dedup();
         DiffInfo {
-            diffs: new_diffs,
+            diffs: self.diffs,
             total_keys: self.total_keys,
         }
     }
 }
 
 fn json_two_diff(data1: &Value, data2: &Value) -> DiffInfo {
-    json_deep_diff(data1, data2, String::from(""), 0).merge(&mut json_deep_diff(
-        data2,
-        data1,
-        String::from(""),
-        0,
-    ))
+    let diff1 = json_deep_diff(data1, data2, String::from(""), 0);
+    let diff2 = json_deep_diff(data2, data1, String::from(""), 0);
+    diff1.merge(diff2)
 }
 
 fn json_deep_diff(data1: &Value, data2: &Value, prefix: String, start_keys: usize) -> DiffInfo {
-    let dne = json!(DNE);
-    let mut keys = vec![];
+    let local_dne = json!(DNE);
+    let mut keys = Vec::new();
     let mut total = start_keys;
 
     match (data1, data2) {
         (Value::Object(map1), Value::Object(map2)) => {
             for (key, val) in map1.iter() {
-                let val2 = map2.get(key).unwrap_or(&dne);
-                let mut next_diff = json_deep_diff(val, val2, format!("{prefix}/{key}"), 1);
-                keys.append(&mut next_diff.diffs);
+                let val2 = map2.get(key).unwrap_or(&local_dne);
+                let next_diff = json_deep_diff(val, val2, format!("{prefix}/{key}"), 1);
+                keys.extend(next_diff.diffs);
                 total += next_diff.total_keys;
             }
         }
         (Value::Array(vec1), Value::Array(vec2)) => {
             for (key, val) in vec1.iter().enumerate() {
-                let val2 = vec2.get(key).unwrap_or(&dne);
-
-                let mut next_diff = json_deep_diff(val, val2, format!("{prefix}/{key}"), 1);
-                keys.append(&mut next_diff.diffs);
+                let val2 = vec2.get(key).unwrap_or(&local_dne);
+                let next_diff = json_deep_diff(val, val2, format!("{prefix}/{key}"), 1);
+                keys.extend(next_diff.diffs);
                 total += next_diff.total_keys;
             }
         }
@@ -85,32 +71,26 @@ fn json_deep_diff(data1: &Value, data2: &Value, prefix: String, start_keys: usiz
 
 pub fn check(file1: ValidatedFile, file2: ValidatedFile) -> Result<(), Error> {
     if file1.format != file2.format {
-        return Err(Error::InvalidIOFormat(file2.format.to_string()));
+        return Err(Error::InvalidIOFormat(file2.format.clone()));
     }
 
-    let data1: Value =
-        serde_json::from_reader(&file1.file).expect("Expected a properly formatted JSON file");
-    let data2: Value =
-        serde_json::from_reader(&file2.file).expect("Expected a properly formatted JSON file");
+    let data1: Value = serde_json::from_reader(&file1.file)
+        .map_err(|_| Error::InvalidIOFormat(file1.format.clone()))?;
+    let data2: Value = serde_json::from_reader(&file2.file)
+        .map_err(|_| Error::InvalidIOFormat(file2.format.clone()))?;
 
     if data1 == data2 {
-        return Ok(p!("{} and {} are 100% the same.", &file1.name, &file2.name));
+        println!("{} and {} are 100% the same.", &file1.name, &file2.name);
+        return Ok(());
     } else if !data1.is_object() || !data2.is_object() {
-        p!("{} and {} are different types.", &file1.name, &file2.name)
+        println!("{} and {} are different types.", &file1.name, &file2.name);
+        return Ok(());
     }
 
-    // let diff = json_patch::diff(&data1, &data2);
-    let diff = json_deep_diff(&data1, &data2, String::from(""), 0);
-    p!("{:?}", diff);
-
-    // let diff2 = json_deep_diff(&data2, &data1, String::from(""), 0);
-    // p!("{:?}", diff2);
-
-    // todo!();
-
+    let diff = json_two_diff(&data1, &data2);
     let percentage = (diff.diffs.len() as f32 / diff.total_keys as f32) * 100.0;
 
-    p!(
+    println!(
         "{} and {} are {:.1}% different.\nDifferent Keys ({}):\n{:?}",
         &file1.name,
         &file2.name,
